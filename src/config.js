8b2a33739b37da7312fbf8d5083d904be8cf7f4c
ImaniN BetSoftware <imanin@betsoftware.com>
2025-04-24 18:34:54 +0200

    Add configuration file with API keys and URLs for weather and time services

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    // API keys and URLs for various services
+    WEATHER_API_KEY: "b2a5adcct04b33178913oc335f405433",
+    WEATHER_API_URL: "https://api.shecodes.io/weather/v1/current",
+
+    TIME_API_KEY: "q5CXMd35C4C37dP6tkezYFgBWQ5nE6",
+    TIME_API_URL: "https://www.amdoren.com/api/time.php"
+};
+
+export default config;
